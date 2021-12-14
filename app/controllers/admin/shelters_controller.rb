f108c2ca8042ab6b979906dd399eed1c7e994c17
Kelly Anderson <kean9528@colorado.edu>
2021-12-13 18:36:52 -0700

    write test for pending applications

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -1,5 +1,5 @@
 class Admin::SheltersController <  ApplicationController
   def index
-    @shelters = Shelter.all.sort_reverse_alpha
+    @shelters = Shelter.sort_reverse_alpha
   end
 end
--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -5,9 +5,13 @@ RSpec.describe 'the shelters index' do
     @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
     @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
-    @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-    @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-    @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    @pet1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    @pet2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    @pet3 = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    @application1 = Application.create!(name:'Kelly', address: '123 test st', city: 'Boulder', state: 'CO', zip: '80016')
+    @pet_application = PetApplication.create!(pet_id: @pet1.id, application_id: @application1.id)
+    @application2 = Application.create!(name:'Kara', address: '1234 address', city: 'Denver', state: 'CO', zip: '80016')
+    @pet_application2 = PetApplication.create!(pet_id: @pet2.id, application_id: @application2.id)
   end
 
   it 'lists all the shelter names' do
@@ -17,5 +21,9 @@ RSpec.describe 'the shelters index' do
     expect(page).to have_content(@shelter_3.name)
     expect(@shelter_2.name).to appear_before(@shelter_3.name)
     expect(@shelter_3.name).to appear_before(@shelter_1.name)
+  end
+
+  it 'I see the name of every shelter that has a pending application' do
+    expect(page).to have_content("Shelters with pending Applications: #{@shelter1.id}")
   end
 end
